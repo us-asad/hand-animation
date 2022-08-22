32527b113c092dac194b28a2938bf7b1723641b8
Asad <usasad99@gmail.com>
2022-08-22 23:57:25 +0500

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function App() {
   const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(prev => {
-        console.log(prev > 10)
-        if (time > 10) {
-          clearInterval(interval);
-        } else {
-          return prev + 1
-        }
-      });
-    }, 1000);
-
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
+  const [disableBtn, setDisableBtn] = useState(false);
 
   const restartAnimation = () => {
-    setTime(0);
+    setDisableBtn(prev => !prev);
+    const interval = setInterval(() => {
+      if (time < 10) {
+        setTime(prev => {
+          if (time > 10) {
+            clearInterval(interval);
+          } else {
+            return prev + 1
+          }
+        });
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
   }
 
   return (
@@ -39,7 +35,7 @@ export default function App() {
         <div className={`bg-gray-600 mb-[13px] ml-[-24px] h-[346px] w-[500px] absolute duration-[3.4s] right-[124px] ease ${time > 5.5 ? "!right-[-800px]" : ""}`}>aaaa</div>
         <img src="/assets/hand.png" alt="hand" className={`w-[200px] absolute top-0 animate-hand duration-[2.5s] ${time > 2 && time < 5.5 ? "!right-0" : ""}`} />
       </div>
-      <button disabled={time < 5.5} onClick={restartAnimation} className="block disabled:opacity-50 disabled:hover:text-white disabled:hover:bg-green-500 disabled:cursor-not-allowed mt-3 p-2 bg-green-500 text-white hover:text-green-500 hover:bg-white border-2 border-green-500 duration-200 ">restart animation</button>
+      <button disabled={disableBtn} onClick={restartAnimation} className="active:scale-90 disabled:active:scale-100 block disabled:opacity-50 disabled:hover:text-white disabled:hover:bg-green-500 disabled:cursor-not-allowed mt-3 p-2 bg-green-500 text-white hover:text-green-500 hover:bg-white border-2 border-green-500 duration-200 ">start animation</button>
     </div>
   )
 }
