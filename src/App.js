f1e241e05808d795e6196b8bd0d29eb4ea99999e
Asad <usasad99@gmail.com>
2022-08-23 00:06:43 +0500

    added change #2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import { useState } from "react"
 
 export default function App() {
   const [time, setTime] = useState(0);
-  const [disableBtn, setDisableBtn] = useState(false);
 
   const restartAnimation = () => {
-    setDisableBtn(prev => !prev);
     const interval = setInterval(() => {
       if (time < 10) {
         setTime(prev => {
@@ -24,7 +22,7 @@ export default function App() {
   return (
     <div className="w-full h-screen grid place-content-center">
       <div className="w-[800px] bg-gray-300 flex items-center relative overflow-hidden">
-        <div className="w-[200px] relative z-[2]">
+        <div className="w-[200px] relative z-[2]" onClick={restartAnimation}>
           <img src="/assets/a.png" alt="a" width={200} />
         </div>
         <div className={`bg-gray-600 mb-[13px] ml-[-24px] h-[346px] w-[500px] overflow-hidden relative duration-[2.35s] right-[497px] ${time > 5.2 ? "!right-0" : ""}`}>
@@ -35,7 +33,6 @@ export default function App() {
         <div className={`bg-gray-600 mb-[13px] ml-[-24px] h-[346px] w-[500px] absolute duration-[3.4s] right-[124px] ease ${time > 5.5 ? "!right-[-800px]" : ""}`}>aaaa</div>
         <img src="/assets/hand.png" alt="hand" className={`w-[200px] absolute top-0 animate-hand duration-[2.5s] ${time > 2 && time < 5.5 ? "!right-0" : ""}`} />
       </div>
-      <button disabled={disableBtn} onClick={restartAnimation} className="active:scale-90 disabled:active:scale-100 block disabled:opacity-50 disabled:hover:text-white disabled:hover:bg-green-500 disabled:cursor-not-allowed mt-3 p-2 bg-green-500 text-white hover:text-green-500 hover:bg-white border-2 border-green-500 duration-200 ">start animation</button>
     </div>
   )
 }
